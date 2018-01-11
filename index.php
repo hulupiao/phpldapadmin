a438cc68405599ed8509a40dd6acf5bb55ecb75a
hanjiafeng <jiafeng.han@ret.cn>
2018-01-11 18:23:38 +0800

    fork phpldapadmin

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+// $Header$
+
+/**
+ * @package phpLDAPadmin
+ */
+#error_reporting(0); 
+#ini_set('display_errors', 0);
+# You should secure your PLA by making the htdocs/ your docroot.
+header('Location: htdocs/index.php');
+die();
+?>
